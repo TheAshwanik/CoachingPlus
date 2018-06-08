b6bb0ca4d376e0e6d793855e552f4337124c7242
Ashwani Kumar <aryan.ashwani@gmail.com>
2018-06-08 23:47:29 +0100

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoachingPlus::Application.config.secret_token = '520c5721ab32118936100b997bc0174351ea93ca8cb75be2dfacc1c5d0d955c08b1d36d629040bedfa88f49349e31621581cef4ad1b0adca9cbb29d515e4bcea'
